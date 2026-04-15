63136bd68b74464548fdb0cb0fb1e7de08bed9f6
suraiyakabir <hamidahkabir12@gmail.com>
2026-04-15 15:16:48 +0600

    pushing

--- client/412-lab-demo-portfolio/src/App.jsx
+++ client/412-lab-demo-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./index.css";
-
+const API_URL = import.meta.env.VITE_API_URL;
 function App() {
  const [projects, setProjects] = useState([]);
  const [formData, setFormData] = useState({
@@ -10,7 +10,7 @@ function App() {
  });
 
  useEffect(() => {
-   fetch("http://localhost:5000/projects")
+   fetch(`${API_URL}/projects`)
      .then((res) => res.json())
      .then((data) => setProjects(data))
      .catch((err) => console.error(err));
@@ -26,7 +26,7 @@ function App() {
  const handleSubmit = async (e) => {
    e.preventDefault();
 
-   const response = await fetch("http://localhost:5000/contact", {
+   const response = await fetch(`${API_URL}/contact`, {
      method: "POST",
      headers: {
        "Content-Type": "application/json"
